43196fe401429e5c084d228e5226ccc94d894cd6
ys20122 <yisamgye.s240250@ggh.goe.go.kr>
2025-07-09 14:13:15 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('나의 첫 웹서비스 만들기!')
+name = st.text_imput('이름을 입력하세요')
+menu = st.selectbox('가장 많이 쓰는 앱은? :', ['유튜브','인스타', '카톡'])
+time = st.slider('하루 사용 시간은?', 0, 12, 3)
+if st.button('나의 디지털 습관') : 
+  st.write(f'{name}님! {menu}를 {time}시간 사용 중이시군요. 균형 잡힌 사용이 중요해요!')
